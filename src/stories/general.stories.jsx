defb9836ddbe36678eee0c231e7a5a727366186b
christopher-codoxx <christopher.santana@codoxx.com>
2022-04-07 12:35:25 -0400

    Added spining animation

---
+++ src/stories/general.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default {
+        title: "Loading"
+    }
+export  const LoadingScreen = () => {
+    return <div className="loading-screen">
+            <img src="https://tedwaffl.sirv.com/Activa/spinner.png"  className="loading-image" alt="" />
+        </div>
+}
