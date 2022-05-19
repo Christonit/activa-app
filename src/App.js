10435d99101206bcce9129e5489657937f23d17f
Christonit <christopher.alesan@gmail.com>
2022-05-19 12:43:07 -0400

    Fixed animation for sheet-drawer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Onboarding } from "./views/onboarding";
+import { MainScreen } from "./views/principal";
 function App() {
   return (
     <div className="App">
-      <Onboarding/>
+      <MainScreen />
     </div>
   );
 }
