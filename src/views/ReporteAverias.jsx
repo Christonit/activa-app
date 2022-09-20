22c01463136a21a4718c8f0214a7524c18323bcd
Christonit <christopher.alesan@gmail.com>
2022-09-20 05:56:39 -0400

    Updates on Monday 19 Sept

--- src/views/ReporteAverias.jsx
+++ src/views/ReporteAverias.jsx
@@ -20,6 +20,49 @@ export const ReportesAveria = () => {
                     </div>
 
                     <div className="notif-content no-border">
+                        <div className="collapsable-box  active">
+                            <div className="collabable-row ">
+                                <div className="">
+                                    <p className="text-smallest medium mb-8">
+                                        RESUMEN DEL PEDIDO: <span className=" success">REF. 678590</span>
+                                    </p>
+                                    <p className="text bold mb-0">Canales pixelados (todos)</p>
+                                </div>
+
+                                <button className="collapsable-btn inactive material-icons">keyboard_arrow_down</button>
+                                <button className="collapsable-btn active material-icons">keyboard_arrow_up</button>
+                            </div>
+
+                            <div className="collapsable-content">
+                                <div className="collabable-row py-8 mt-8 d-block">
+                                    <p className="text size-14 mb-4">
+                                        <b>Prioridad:</b> Normal <span className="red">!</span>
+                                    </p>
+                                    <p className="text size-14 mb-4">
+                                        <b>Estado:</b> <span className="red">Esperando aprobación.</span>
+                                    </p>
+                                    <p className="text size-14  mb-4 bold">
+                                        Número de cuenta:<span className="blue"> 0010014243</span>
+                                    </p>
+                                    <p className="text size-14 mb-4">
+                                        <b>Fecha y hora del reporte:</b> 9 de Junio 2022, 4:38pm
+                                    </p>
+                                    <p className="text size-14 mb-4">
+                                        <b>Técnico asignado:</b> Juan Pérez
+                                    </p>
+                                    <p className="text size-14">
+                                        <b>Tiempo estimado de resolución:</b> 48 Horas
+                                    </p>
+
+                                    <p className="text size-14 bold mb-0">Descripción:</p>
+                                    <p className="text size-14 mb-0">
+                                        Descripción: La calidad de la imagen de todos los canales es muy mala y se ve
+                                        cortada.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
                         <div className="collapsable-box alternative">
                             <div className="collabable-row ">
                                 <div className="">
@@ -62,6 +105,7 @@ export const ReportesAveria = () => {
                                 </div>
                             </div>
                         </div>
+
                         <div className="collapsable-box alternative active">
                             <div className="collabable-row ">
                                 <div className="">
--- src/views/principal.jsx
+++ src/views/principal.jsx
@@ -1,187 +1,143 @@
-import { React } from "react";
-import '../sass/main.scss';
-import { BasicaBanner } from "../components/banners";
+import {React, useEffect} from "react";
+import "../sass/main.scss";
+import {BasicaBanner} from "../components/banners";
 export const MainScreen = ({slide_active = false}) => {
-    return <section className="view-container">
+    useEffect(() => {
+        const actions = document.querySelector("#main-actions-access");
+        const el_position = actions.offsetTop;
+        const sidesheet = document.querySelector("#payment-drawers");
+        sidesheet.style.top = 164 + el_position + "px";
+    }, []);
 
-        {slide_active && <div className="overlay"></div>}
-        <div className="nav-header home-screen">
-            <button className="app-bar-button home-screen-button">
-                <img src="https://tedwaffl.sirv.com/Activa/menu-copy.svg" alt="" />
-            </button>
-
-
-            <div className="home-screen-button-container">
-                <button className="app-bar-button mr-16">
-                    <img src="https://tedwaffl.sirv.com/Activa/notifications.svg" alt="" />
+    return (
+        <section className="view-container">
+            {slide_active && <div className="overlay"></div>}
+            <div className="nav-header home-screen">
+                <button className="app-bar-button home-screen-button">
+                    <img src="https://tedwaffl.sirv.com/Activa/menu-copy.svg" alt="" />
                 </button>
-                <button className="app-bar-button">
-                    <span className="notif-counter">9</span>
-                    <img src="https://tedwaffl.sirv.com/Activa/bag.svg" alt="" />
-                </button>
+
+                <div className="home-screen-button-container">
+                    <button className="app-bar-button mr-16">
+                        <img src="https://tedwaffl.sirv.com/Activa/notifications.svg" alt="" />
+                    </button>
+                    <button className="app-bar-button">
+                        <span className="notif-counter">9</span>
+                        <img src="https://tedwaffl.sirv.com/Activa/bag.svg" alt="" />
+                    </button>
+                </div>
             </div>
-            
-        </div>
 
+            <div className="view-body">
+                <BasicaBanner />
 
-        <div className="view-body">
-
-            <BasicaBanner/>
-
-            <div className="user-information">
-                <div className="user-information-picture">
-                    <div className="user-information-picture-container">
-                        <img src="https://tedwaffl.sirv.com/Activa/152319944-close-up-headshot-portrait-of-smiling-vietnamese-young-woman-look-at-camera-talk-on-video-call-profi.jpg" alt="" />
+                <div className="user-information">
+                    <div className="user-information-picture">
+                        <div className="user-information-picture-container">
+                            <img
+                                src="https://tedwaffl.sirv.com/Activa/152319944-close-up-headshot-portrait-of-smiling-vietnamese-young-woman-look-at-camera-talk-on-video-call-profi.jpg"
+                                alt=""
+                            />
+                        </div>
+                        <button className="user-information-button">
+                            <img src="https://tedwaffl.sirv.com/Activa/edit.svg" alt="" />
+                        </button>
                     </div>
-                    <button className="user-information-button"><img src="https://tedwaffl.sirv.com/Activa/edit.svg" alt="" /></button>
+
+                    <h1 className="user-information-title">¡Hola Carlos, Bienvenido!</h1>
+
+                    <p className="label-color text-small">carlos.perez@gmail.com</p>
+                </div>
+                <div className="main-actions-access" id="main-actions-access">
+                    <button className="main-action-btn">
+                        <span className="main-action-icon">
+                            <img src="https://tedwaffl.sirv.com/Activa/balance.svg" alt="" />
+                        </span>
+                        <span className="main-action-label">Balance</span>
+                    </button>
+                    <button className="main-action-btn">
+                        <span className="main-action-icon">
+                            <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
+                        </span>
+                        <span className="main-action-label">Pagos</span>
+                    </button>
+                    <button className="main-action-btn">
+                        <span className="main-action-icon">
+                            <img src="https://tedwaffl.sirv.com/Activa/offers.svg" alt="" />
+                        </span>
+                        <span className="main-action-label">Ofertas</span>
+                    </button>
+                    <button className="main-action-btn">
+                        <span className="main-action-icon">
+                            <img src="https://tedwaffl.sirv.com/Activa/averias.svg" alt="" />
+                        </span>
+                        <span className="main-action-label">Averías</span>
+                    </button>
                 </div>
 
-                <h1 className="user-information-title">¡Hola Carlos, Bienvenido!</h1>
+                <div id="payment-drawers" className={`sheet-drawer ${slide_active ? "active" : ""}`}>
+                    <div className="sheet-drawer-header">
+                        <button className="drawer-button">
+                            <hr />
+                        </button>
+                        <h3 className="text bold text-center mb-12">Número de cuenta:</h3>
+                        <input type="text" className="form-control sheet-drawer-input" name="service" />
+                    </div>
 
-                <p className="label-color text-small">carlos.perez@gmail.com</p>
-
-            </div>
-
-            <div className="available-money">
-                <p className="label-color text-small mb-8">carlos.perez@gmail.com</p>
-                <h2 className="title success mb-0">RD$ 198.40</h2>
-
-                <span className="available-money-label">Billetera</span>
-
-            </div>
-
-            <div className="main-actions-access">
-                <button className="main-action-btn">
-                    <span className="main-action-icon">
-                        <img src="https://tedwaffl.sirv.com/Activa/balance.svg" alt="" />
-                        </span>
-                    <span className="main-action-label">Balance</span>
-                </button>
-                <button className="main-action-btn">
-                    <span className="main-action-icon">
+                    <div className="service-item">
                         <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
-                    </span>
-                    <span className="main-action-label">Pagos</span>
-                </button>
-                <button className="main-action-btn">
-                    <span className="main-action-icon">
-                        <img src="https://tedwaffl.sirv.com/Activa/offers.svg" alt="" />
-                    </span>
-                    <span className="main-action-label">Ofertas</span>
-                </button>
-                <button className="main-action-btn">
-                    <span className="main-action-icon">
-                        <img src="https://tedwaffl.sirv.com/Activa/averias.svg" alt="" />
-                    </span>
-                    <span className="main-action-label">Averías</span>
-                </button>
+                        <div className="ms-3">
+                            <span className="service-item-title">Activate GO</span>
+
+                            <span className="service-item-text">
+                                Disponible por:
+                                <span className="service-item-data">2 dias</span>
+                            </span>
+                        </div>
+
+                        <button class="material-icons">navigate_next</button>
+                    </div>
+                    <div className="service-item">
+                        <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
+                        <div className="ms-3">
+                            <span className="service-item-title">Activate GO</span>
+
+                            <span className="service-item-text">
+                                Disponible por:
+                                <span className="service-item-data">2 dias</span>
+                            </span>
+                        </div>
+
+                        <button class="material-icons">navigate_next</button>
+                    </div>
+                    <div className="service-item">
+                        <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
+                        <div className="ms-3">
+                            <span className="service-item-title">Activate GO</span>
+
+                            <span className="service-item-text">
+                                Disponible por:
+                                <span className="service-item-data">2 dias</span>
+                            </span>
+                        </div>
+
+                        <button class="material-icons">navigate_next</button>
+                    </div>
+                    <div className="service-item">
+                        <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
+                        <div className="ms-3">
+                            <span className="service-item-title">Activate GO</span>
+
+                            <span className="service-item-text">
+                                Disponible por:
+                                <span className="service-item-data">2 dias</span>
+                            </span>
+                        </div>
+
+                        <button class="material-icons">navigate_next</button>
+                    </div>
+                </div>
             </div>
-
-            <div className={`sheet-drawer ${slide_active ? 'active' : ''}`}>
-
-                <div className="sheet-drawer-header">
-                    <button className="drawer-button"><hr /></button>
-                    <h3 className="text bold text-center mb-12">Número de cuenta:</h3>
-                    <input type="text" className="form-control sheet-drawer-input" name="service"/>
-                </div>
-
-                <div className="service-item">
-                    <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
-                    <div className="ms-3">
-                        <span className="service-item-title">
-                            Activate GO
-                        </span>
-
-                        <span className="service-item-text">
-                            Disponible por:
-                            <span className="service-item-data">
-                                2 dias
-                            </span>
-                        </span>
-
-                        
-                    </div>
-
-                    <button class="material-icons">
-                        navigate_next
-                    </button>
-                </div>
-                <div className="service-item">
-                    <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
-                    <div className="ms-3">
-                        <span className="service-item-title">
-                            Activate GO
-                        </span>
-
-                        <span className="service-item-text">
-                            Disponible por:
-                            <span className="service-item-data">
-                                2 dias
-                            </span>
-                        </span>
-
-                        
-                    </div>
-
-                    <button class="material-icons">
-                        navigate_next
-                    </button>
-                </div>
-                <div className="service-item">
-                    <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
-                    <div className="ms-3">
-                        <span className="service-item-title">
-                            Activate GO
-                        </span>
-
-                        <span className="service-item-text">
-                            Disponible por:
-                            <span className="service-item-data">
-                                2 dias
-                            </span>
-                        </span>
-
-                        
-                    </div>
-
-                    <button class="material-icons">
-                        navigate_next
-                    </button>
-                </div>
-                <div className="service-item">
-                <img src="https://tedwaffl.sirv.com/Activa/combined-shape.svg" alt="" />
-                    <div className="ms-3">
-                        <span className="service-item-title">
-                            Activate GO
-                        </span>
-
-                        <span className="service-item-text">
-                            Disponible por:
-                            <span className="service-item-data">
-                                2 dias
-                            </span>
-                        </span>
-
-                        
-                    </div>
-
-                    <button class="material-icons">
-                        navigate_next
-                    </button>
-                </div>
-
-            </div>
-            <h1 className="title mb-16">Billetera</h1>
-            
-            <div className="d-flex align-items-center">
-                <p className="text mb-0">Balance: <b className="red ml-4">RD$ 25.75</b> </p> 
-            </div>
-
-
-
-        </div>
-        
-        
-
-    </section>
-}
+        </section>
+    );
+};
